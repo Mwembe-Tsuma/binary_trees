23434d68e36bee22e6d285d60dda84106305b1b5
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-08-30 11:00:45 +0000

    9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -15,7 +15,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree)
 	{
 		left_tree = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-		riht_tree = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		right_tree = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 
 		return ((left_tree > right_tree) ?
 			(left_tree) : (right_tree));
