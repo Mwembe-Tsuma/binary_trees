a9a179088396455051035ae090c56e8a99e79c8a
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-08-30 13:06:50 +0000

    18-binary_tree_uncle.c

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+  *binary_tree_uncle - check for uncle node
+  *@node: pointer to the node to check
+  *
+  *Return: Pointer to uncle node or NULL otherwise
+  */
+
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL ||
+			node->parent->parent == NULL)
+	{
+		return (NULL);
+	}
+
+	if (node->parent->parent->left == node->parent)
+		return (node->parent->parent->right);
+
+	return (node->parent->parent->left);
+}
