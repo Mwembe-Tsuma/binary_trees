c00efa15db0d4f319348b8345e66984aa3661b1d
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-08-30 12:57:14 +0000

    17-binary_tree_sibling.c

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+  *binary_tree_sibling - check for sibling node
+  *@node: pointer to the node to check
+  *
+  *Return: Pointer to sibling node or NULL otherwise
+  */
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL)
+	{
+		return (NULL);
+	}
+
+	if (node->parent->left == node)
+		return (node->parent->right);
+
+	return (node->parent->left);
+}
