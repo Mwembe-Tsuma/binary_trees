0330ea182312ac627c88943ea6d848325bc0fda3
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-09-01 06:32:20 +0000

    100-binary_trees_ancestor.c

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -7,8 +7,8 @@
   *Return: Pointer to ancestor node or NULL otherwise
   */
 
-binary_tree_t *binary_trees_ancestor
-	(const binary_tree_t *first, const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+		const binary_tree_t *second)
 {
 	binary_tree_t *pop, *mum;
 
